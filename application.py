2d7345b23137455a83de7c1a84ace986d779f6ed
Abhilash Srivastava <abhi.srivastava456@gmail.com>
2019-07-20 18:49:17 +0530

    updated

--- application.py
+++ application.py
@@ -17,7 +17,7 @@ from sqlalchemy.orm import scoped_session,sessionmaker
 from passlib.hash import sha256_crypt
 from flask_mysqldb import MySQL
 import yaml
-from forms import StatusForm,SearchForm,RegistrationForm, LoginForm ,AdminLoginForm
+from forms import jobVacancy,StatusForm,SearchForm,RegistrationForm, LoginForm ,AdminLoginForm
 import random
 import os
 import urllib.request
@@ -46,11 +46,28 @@ mysql=MySQL(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'Login'
-    
+
+#HOME pages    
 @app.route('/')
 def home():
-    return render_template('home.html',title='HomePage')
+    return render_template('home.html',title='Home')
+@app.route('/adminHome')
+def adminHome():
+    data=db.execute("SELECT jobId,count(jobId) FROM candid GROUP BY jobId;")
+    #data=db.execute("SELECT j.job,count(c.jobId) FROM candid as c RIGHT JOIN jobVac as j ON c.jobId=j.jobId GROUP BY c.jobId;")
+    fetchData=data.fetchall();
+    graphdata=[]
+    for ele in fetchData:
+        graphdata.append([str(ele[0]),ele[1]])
+    data1=db.execute("SELECT jobId,job,noOfVac FROM jobVac;")
+    fetchData1=data1.fetchall()
+    graphdata1=[]
+    for ele in fetchData1:
+        graphdata1.append(['('+str(ele[0])+')'+ele[1],ele[2]])
+    return render_template('mainDashboard.html', title='adminHome',graphdata=graphdata,graphdata1=graphdata1)
 
+
+#Registration Page
 @app.route('/register',methods=['GET','POST'])
 def registration():
     form=RegistrationForm()
@@ -59,7 +76,7 @@ def registration():
         email=request.form.get("email")
         jId=request.form.get("jobId")
         notice=request.form.get("notice")
-        image_file=request.form.get("image")
+        #image_file=request.form.get("image")
         resume=request.form.get("resume")
         phno=request.form.get("phno")
         source=request.form.get("source")
@@ -69,8 +86,8 @@ def registration():
         secure_password=sha256_crypt.encrypt(str(password))
         
         if password==confirm:
-            db.execute(f"INSERT INTO candid(name,email,jobId,image,phno,password,resume,noticep,skills,source) VALUES(:name,:email,:jId,:image,:phno,:password,:resume,:noticep,:skills,:source)",
-                                           {"name":name,"email":email,"jId":jId,"image":image_file,"phno":phno,"password":secure_password,"resume":resume,"noticep":notice,"skills":skills,"source":source})
+            db.execute(f"INSERT INTO candid(name,email,jobId,phno,password,resume,noticep,skills,source) VALUES(:name,:email,:jId,:phno,:password,:resume,:noticep,:skills,:source)",
+                                           {"name":name,"email":email,"jId":jId,"phno":phno,"password":secure_password,"resume":resume,"noticep":notice,"skills":skills,"source":source})
             db.commit()
             flash(f'Account created for {form.username.data}!','success')
             return redirect(url_for('Login'))
@@ -105,6 +122,9 @@ def Login():
                 flash("incorrect password","danger")
                 return render_template("login.html",form=form)
     return render_template('login.html',title='Login',form=form)
+
+
+#HR login with graph data
 @app.route('/adminlogin',methods=['GET','POST'])
 def AdLogin():
     form=AdminLoginForm()
@@ -113,6 +133,19 @@ def AdLogin():
         password=request.form.get("password")
         adIddata=db.execute("SELECT adminId FROM admins WHERE adminId=:adminId",{"adminId":adminId}).fetchone()
         passworddata=db.execute("SELECT password FROM admins WHERE adminId=:adminId",{"adminId":adminId}).fetchone()[0]
+        #for applied
+        data=db.execute("SELECT jobId,count(jobId) FROM candid GROUP BY jobId;")
+        fetchData=data.fetchall()
+        graphdata=[]
+        for ele in fetchData:
+            graphdata.append([str(ele[0]),ele[1]])
+            
+        #for vacancy
+        data1=db.execute("SELECT jobId,job,noOfVac FROM jobVac;")
+        fetchData1=data1.fetchall()
+        graphdata1=[]
+        for ele in fetchData1:
+            graphdata1.append(['('+str(ele[0])+')'+ele[1],ele[2]])
         if adIddata is None:
             flash("No adminId","danger")
             return render_template("adminlogin.html",form=form)
@@ -121,29 +154,28 @@ def AdLogin():
                 session['loggedin']=True
                 session['admin']=request.form['adminId']
                 flash("You are now logged in",'success')
-                return redirect(url_for('adminHome'))
+                return render_template("mainDashboard.html",graphdata=json.dumps(graphdata),graphdata1=json.dumps(graphdata1))
             else:
                 flash("incorrect password","danger")
                 return render_template("adminlogin.html",form=form)
     return render_template('adminlogin.html',title='Login',form=form)
-@app.route('/adminHome')
-def adminHome():
-    return render_template('adminHome.html', title='Home')
+
 #dashboard to enter the status.
 @app.route('/dashboard')
 def dashboard():
     return render_template('profilePage.html', title='dashboard')
 @app.route('/users')
 def info():
-    cur=mysql.connection.cursor()
-    resultValue=cur.execute("select * from candid;")
-    if resultValue>0:
-        userDetails=cur.fetchall()
-        if(userDetails[0][7].endswith('/view?usp=sharing')):
-            userR=userDetails[0][7][:-17]+'?export=download'
-            return render_template('users.html',userDetails=userDetails,userR=userR)
-
-
+    #cur=mysql.connection.cursor()
+    resultValue=db.execute("select * from candid;")
+    userDetails=resultValue.fetchall()
+    db.commit()
+    if(userDetails[0][7].endswith('/view?usp=sharing')):
+        userR=userDetails[0][7][:-17]+'?export=download'
+        return render_template('users.html',userDetails=userDetails,userR=userR)
+    return render_template('users.html',userDetails=userDetails)
+    #return redirect(url_for('adminHome'))
+#logins
 @app.route("/logoutcandid")
 def logoutcandid():
     if 'logged_in' in session:
@@ -158,6 +190,9 @@ def logoutadmin():
         flash("You have been logged out!",'success')
     #gc.collect()
     return redirect(url_for('AdLogin'))
+
+
+
 @app.route('/prof')
 #@login_required
 def prof():
@@ -173,24 +208,17 @@ def prof():
             return render_template('profilePage.html',userDetails=userDetails,statDetails=statDetails,file_name=file_name)
         return render_template('profilePage.html',userDetails=userDetails,statDetails=statDetails,file_name=file_name)
 
-@app.route('/adprof',methods=['GET','POST'])
+#profile list and resume download
+@app.route('/adprof',methods=['POST'])
 def adprof():
+    form=StatusForm()
     if 'loggedin' in session:
-        rf=request.form
-        #flash(rf,'danger')
-        #flash(rf.keys())
-        userDetails=db.execute("SELECT * from candid where id=:id;",{"id":11}).fetchone()
-        statDetails=db.execute("SELECT * from candidStat where id=:id;",{"id":11}).fetchone()
-        for key in rf.keys():
-            data=key
-            flash(data)
-            data_dic=json.loads(data)
-            flash(data_dic['val'])
-            userDetails=db.execute("SELECT * from candid where id=:id;",{"id":data_dic['val']}).fetchone()
-            flash(userDetails)
-            statDetails=db.execute("SELECT * from candidStat where id=:id;",{"id":userDetails[0]}).fetchone()
-            return render_template('profilePage.html',userDetails=userDetails,statDetails=statDetails)
-        return render_template('profilePage.html',userDetails=userDetails,statDetails=statDetails)
+        rf=request.get_json(silent=True)
+        userDetails=db.execute("SELECT * from candid where id=:id;",{"id":rf['val']}).fetchone()
+        statDetails=db.execute("SELECT * from candidStat where id=:id;",{"id":userDetails[0]}).fetchone()
+        
+        return render_template('profilePageadm.html',userDetails=userDetails,statDetails=statDetails,form=form)
+    #return render_template('profilePage.html',userDetails=userDetails,statDetails=statDetails)
 
            
 @app.route('/stat',methods=['GET','POST'])
@@ -220,6 +248,28 @@ def statEnt():
             return render_template('dashboard.html',details=details)
     return render_template('statent.html',form=form)
 
+#vacancy entry
+@app.route("/vac",methods=['GET','POST'])
+def jobVac():
+    form=jobVacancy()
+    if request.method=='POST':
+        jobId=request.form.get("jobId")
+        job=request.form.get("job")
+        no=request.form.get("no")
+        count=db.execute("SELECT COUNT(jobId) from jobVac WHERE jobId=:jobId;",{"jobId":jobId}).fetchone()[0]
+        if(count==0):
+            db.execute("INSERT INTO jobVac(jobId,job,noOfVac) VALUES(:jobId,:job,:no);",{"jobId":jobId,"job":job,"no":int(no)})
+            db.commit()
+            flash("Entry Done!",'success')
+        else:
+            db.execute("UPDATE jobVac SET noOfVac=:no WHERE jobId=:jobId;",{"no":no,"jobId":jobId})
+            flash("updated",'success')
+        #userDetails=db.execute("SELECT * from candid where id=:id;",{"id":id}).fetchone()
+        #statDetails=db.execute("SELECT * from candidStat where id=:id;",{"id":userDetails[0]}).fetchone()
+        return redirect(url_for('adminHome'))#render_template('profilePageadm.html')#,userDetails=userDetails,statDetails=statDetails)
+    return render_template('jobVc.html',form=form)
+
+
 #filters
 @app.route("/ihome", methods=['GET', 'POST'])
 def search():
@@ -233,7 +283,7 @@ def search():
         rounds=request.form.get('rounds')
         stages=request.form.get('stages')
         if(skills!=None and jobId=="NULL" and stages=="NULL" and rounds=="NULL"):
-            result=db.execute(f"SELECT c.id,c.name,c.email,c.phno,s.r1,s.r2,s.r3,s.r4,s.hr,s.os,s.joined FROM candid as c RIGHT JOIN candidStat as s ON c.id=s.id WHERE s.skills like :skills;",{"skills":'%'+skills+'%'})
+            result=db.execute(f"SELECT c.id,c.name,c.email,c.phno,s.r1,s.r2,s.r3,s.r4,s.hr,s.os,s.joined FROM candid as c RIGHT JOIN candidStat as s ON c.id=s.id WHERE c.skills like :skills;",{"skills":'%'+skills+'%'})
             details=result.fetchall()
             return render_template('dashboard.html',details=details)
         elif(skills!=None and jobId=="NULL" and stages!="NULL" and rounds!="NULL"):
@@ -258,7 +308,28 @@ def search():
             return render_template('dashboard.html',details=details)
     return render_template('searchBar.html', form=form, result=result)
 
+#graph clickable functionality
+@app.route('/graphFilters',methods=['POST'])
+def graphFilters():
+    data=request.get_json(silent=True)
+    #data=data['val'][6:]
+    dataId=data['val']
+    result=db.execute(f"SELECT c.id,c.name,c.email,c.phno,s.r1,s.r2,s.r3,s.r4,s.hr,s.os,s.joined FROM candid as c RIGHT JOIN candidStat as s ON c.id=s.id WHERE c.jobId={int(dataId)};")
+    details=result.fetchall()
+    db.commit()
+    return render_template('dashboard.html',details=details)
     
+@app.route('/graphFiltersav',methods=['POST'])
+def graphFiltersav():
+    data=request.get_json(silent=True)
+    dataId=data['val'][1:5]
+    result1=db.execute(f"SELECT noOfVac FROM jobVac WHERE jobId={int(dataId)};")
+    
+    #result=db.execute(f"SELECT c.id,c.name,c.email,c.phno,s.r1,s.r2,s.r3,s.r4,s.hr,s.os,s.joined FROM candid as c RIGHT JOIN candidStat as s ON c.id=s.id WHERE c.jobId={int(dataId)};")
+    details=result1.fetchall()
+    db.commit()
+    return render_template('jobProf.html',details=details)
+        
     
     
     
